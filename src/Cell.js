6ba168bad6bef171342b690ea5731a678b776e24
Allegra <aukseallegra@gmail.com>
2018-08-07 16:27:51 +0100

    Done.

--- src/Cell.js
+++ src/Cell.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
 
 export default class Cell extends Component {
-  
+
   constructor(props) {
     super(props)
     this.state = {
       color: this.props.color
     }
   }
-  
+
+  handleClick = () => {
+    this.setState({
+      color: this.props.currentCol
+    })
+  }
+
   render() {
     return (
-      <div className="cell" style={{backgroundColor: this.state.color}}>
+      <div className="cell" style={{backgroundColor: this.state.color}}
+        onMouseOver={this.handleClick}>
       </div>
     )
   }
-  
+
 }
--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 
 export default class ColorSelector extends Component {
-  
+
+//from the parent, hence "this.props.selectColor(color)":
+
   makeColorSwatches = () => (
-    ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
-      return <div key={idx} className="color-swatch" style={{backgroundColor: str}}/>
+    ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((color, idx) => {
+      return <div key={idx} className="color-swatch"  style={{backgroundColor: color}}
+        onClick={() => this.props.selectColor(color)}/>
     })
   )
-  
+
   render() {
     return (
       <div id="colorSelector">
@@ -15,5 +18,5 @@ export default class ColorSelector extends Component {
       </div>
     )
   }
-  
+
 }
